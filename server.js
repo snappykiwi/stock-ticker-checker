bd5eaf95c0ea033f6c08ad888bc680b2f6eefc34
Em Coraccio <emcoraccio@gmail.com>
2020-02-15 12:29:48 -0500

    created client folder and server.js file

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+
+const mongoose = require("mongoose");
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+require('dotenv').config();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+
+mongoose.connect(process.env.MONGODB_URI);
+
+app.listen(PORT, function () {
+  console.log(`API Server now listening on PORT ${PORT}!`);
+});
