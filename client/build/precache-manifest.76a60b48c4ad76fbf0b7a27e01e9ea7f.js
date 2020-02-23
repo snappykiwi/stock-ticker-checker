1fdb4d0594537b69f0ab7ab80ac8109263694d9c
Em Coraccio <emcoraccio@gmail.com>
2020-02-23 14:26:28 -0500

    changed path for serving files

---
+++ client/build/precache-manifest.76a60b48c4ad76fbf0b7a27e01e9ea7f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "334673cf9884716453543c0ba0b914e3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ba8c80334d7a1d23f8cf",
+    "url": "/static/css/2.2623327d.chunk.css"
+  },
+  {
+    "revision": "3561338d323396498d48",
+    "url": "/static/css/main.629e3bdd.chunk.css"
+  },
+  {
+    "revision": "ba8c80334d7a1d23f8cf",
+    "url": "/static/js/2.77689627.chunk.js"
+  },
+  {
+    "revision": "549cff9698f0d47a2a839df6aef79c2b",
+    "url": "/static/js/2.77689627.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3561338d323396498d48",
+    "url": "/static/js/main.2fc500c9.chunk.js"
+  },
+  {
+    "revision": "67b8e418968521887aae",
+    "url": "/static/js/runtime-main.821d0026.js"
+  }
+]);
